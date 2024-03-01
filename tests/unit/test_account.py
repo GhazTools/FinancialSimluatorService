e96e834d6b98039d8ce518633ae9a50b75be8a74
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-02-29 20:22:23 -0500

    base test for pre-commit

---
+++ tests/unit/test_account.py
@@ -0,0 +1,22 @@
+"""
+file_name = test_account.py
+Created On: 2024/02/29
+Lasted Updated: 2024/02/29
+Description: _FILL OUT HERE_
+Edit Log:
+2024/02/29
+    - Created file
+"""
+
+# STANDARD LIBRARY IMPORTS
+
+# THIRD PARTY LIBRARY IMPORTS
+
+# LOCAL LIBRARY IMPORTS
+
+
+def test_acccount() -> None:
+    """
+    _summary_
+    """
+    assert 1 == 0 + 1
