27c5cbccfc213070562fdfe781a92337d9c5c7ed
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-02-29 19:43:38 -0500

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,23 @@
+"""
+file_name = wsgi.py
+Created On: 2024/02/29
+Lasted Updated: 2024/02/29
+Description: _FILL OUT HERE_
+Edit Log:
+2024/02/29
+    - Created file
+"""
+
+# STANDARD LIBRARY IMPORTS
+
+# THIRD PARTY LIBRARY IMPORTS
+from uvicorn import run 
+
+# LOCAL LIBRARY IMPORTS
 from app.app import APP
 
+
 application = APP
+
+if __name__ == '__main__':
+    run(application, host="0.0.0.0", port=8000)
